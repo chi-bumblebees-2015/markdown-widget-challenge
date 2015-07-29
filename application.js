d03eb2205d6946f0036e0097ee9156df8ceddc19
Ben DiPaola & Mark Donald Amobi <pair+bdipaola.markamobi@devbootcamp.com>
2015-07-29 11:30:14 -0500

    Solution for release 2

--- application.js
+++ application.js
@@ -9,66 +9,70 @@ function MarkDownWidget(source_id, preview_id){
   source.on("keyup", function(){
     var text = $(this).val();
 
-    //convert text to array
-    var text_array = text.split("");
-
-    //iterate through text and change * to <i> or </i>
-    var bold_counter = 0;
-    var bold_counter2 = 0;
-    var italic_counter = 0;
-    var italic_counter2 = 0;
-    for(var i=0; i < text_array.length; i++){
-      var current_char = text_array[i];
-
-      if(current_char === "*" && text_array[i + 1] === "*" && bold_counter === 0){
-        text_array[i] = "<str";
-        text_array[i+1] = "ong>";
-        bold_counter = 1;
-      }
-      else if(current_char === "*" && text_array[i + 1] === "*" && bold_counter === 1){
-        text_array[i] = "</str";
-        text_array[i+1] = "ong>";
-        bold_counter = 0;
-      }
-
-      else if(current_char === "_" && text_array[i + 1] === "_" && bold_counter2 === 0){
-        text_array[i] = "<str";
-        text_array[i+1] = "ong>";
-        bold_counter2 = 1;
-      }
-      else if(current_char === "_" && text_array[i + 1] === "_" && bold_counter2 === 1){
-        text_array[i] = "</str";
-        text_array[i+1] = "ong>";
-        bold_counter2 = 0;
-      }
-
-      else if(current_char === "*" && text_array[i + 1] != "*" && italic_counter === 0){
-        text_array[i] = "<i>";
-        italic_counter = 1;
-      }
-      else if(current_char === "*" && text_array[i + 1] != "*" && italic_counter === 1){
-        text_array[i] = "</i>";
-        italic_counter = 0;
-      }
-
-      else if(current_char === "_" && text_array[i + 1] != "_" && italic_counter2 === 0){
-        text_array[i] = "<i>";
-        italic_counter2 = 1;
-      }
-      else if(current_char === "_" && text_array[i + 1] != "_" && italic_counter2 === 1){
-        text_array[i] = "</i>";
-        italic_counter2 = 0;
-      }
+    //To go back to original implementation from release 1, remove line 13 and uncomment rest of code
+    preview.html(markdown.toHTML(text))
 
 
+  //   //convert text to array
+  //   var text_array = text.split("");
 
-    }
+  //   //iterate through text and change * to <i> or </i>
+  //   var bold_counter = 0;
+  //   var bold_counter2 = 0;
+  //   var italic_counter = 0;
+  //   var italic_counter2 = 0;
+  //   for(var i=0; i < text_array.length; i++){
+  //     var current_char = text_array[i];
 
-    //convertback to string
-    transformed_text = text_array.join("");
+  //     if(current_char === "*" && text_array[i + 1] === "*" && bold_counter === 0){
+  //       text_array[i] = "<str";
+  //       text_array[i+1] = "ong>";
+  //       bold_counter = 1;
+  //     }
+  //     else if(current_char === "*" && text_array[i + 1] === "*" && bold_counter === 1){
+  //       text_array[i] = "</str";
+  //       text_array[i+1] = "ong>";
+  //       bold_counter = 0;
+  //     }
 
-    //put in preview
-    preview.html(transformed_text);
+  //     else if(current_char === "_" && text_array[i + 1] === "_" && bold_counter2 === 0){
+  //       text_array[i] = "<str";
+  //       text_array[i+1] = "ong>";
+  //       bold_counter2 = 1;
+  //     }
+  //     else if(current_char === "_" && text_array[i + 1] === "_" && bold_counter2 === 1){
+  //       text_array[i] = "</str";
+  //       text_array[i+1] = "ong>";
+  //       bold_counter2 = 0;
+  //     }
+
+  //     else if(current_char === "*" && text_array[i + 1] != "*" && italic_counter === 0){
+  //       text_array[i] = "<i>";
+  //       italic_counter = 1;
+  //     }
+  //     else if(current_char === "*" && text_array[i + 1] != "*" && italic_counter === 1){
+  //       text_array[i] = "</i>";
+  //       italic_counter = 0;
+  //     }
+
+  //     else if(current_char === "_" && text_array[i + 1] != "_" && italic_counter2 === 0){
+  //       text_array[i] = "<i>";
+  //       italic_counter2 = 1;
+  //     }
+  //     else if(current_char === "_" && text_array[i + 1] != "_" && italic_counter2 === 1){
+  //       text_array[i] = "</i>";
+  //       italic_counter2 = 0;
+  //     }
+
+
+
+  //   }
+
+  //   //convertback to string
+  //   transformed_text = text_array.join("");
+
+  //   //put in preview
+  //   preview.html(transformed_text);
   });
 
 }
