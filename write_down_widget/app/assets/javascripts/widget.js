190790d329ad8b2594fa091967f25ee042fd357e
Edwin & Evangeline Garreau <pair+dwin357.egarreau@devbootcamp.com>
2015-07-29 16:15:01 -0500

    working mvc js subtitution widget

---
+++ write_down_widget/app/assets/javascripts/widget.js
@@ -0,0 +1,54 @@
+// model
+var Statement = function(input, output) {
+  this.input = input;
+  this.output = output;
+};
+
+Statement.prototype.processDoubleStarInput = function(data){
+  var boldRegexGlobal = /(\*\*)(.[^\*\*]*)(\*\*)/g;
+  return data.replace(boldRegexGlobal, function(match, p1, p2){
+    return "<strong>" + p2 + "</strong>";
+  }); // closes replace
+};
+
+Statement.prototype.processSingleStarInput = function(data){
+  var starRegexGlobal = /(\*)(.[^\*]*)(\*)/g;
+  return data.replace(starRegexGlobal, function(match, p1, p2){
+    return "<em>" + p2 + "</em>";
+  }); // closes replace
+};
+
+Statement.prototype.processUnderscoreInput = function(data){
+  var underscoreRegexGlobal = /(\_)(.[^\_]*)(\_)/g;
+  return data.replace(underscoreRegexGlobal, function(match, p1, p2){
+    return "<em>" + p2 + "</em>";
+  }); // closes replace
+};
+
+Statement.prototype.markdown_parser = function(){
+  var data = this.input
+  data = this.processDoubleStarInput(data);
+  data = this.processSingleStarInput(data);
+  this.output = this.processUnderscoreInput(data);
+}; // closes markdown_parser
+
+
+
+// controler
+var statement_controller = function(element, model){
+  model.input = element.find('#user-input').val();
+  model.markdown_parser();
+  element.find('#display').html(model.output);
+}; // closes controler
+
+
+
+// view
+$(document).ready(function(){
+ wallie = new Statement("","")
+  $("#widget").on('keyup', function(){
+    statement_controller($(this), wallie);
+  }); // closes keyup
+}); // closes ready
+
+
