a0b938d6a52566d8b1a2c9e12e567c2c88e62ff1
Ben DiPaola & Mark Donald Amobi <pair+bdipaola.markamobi@devbootcamp.com>
2015-07-29 11:13:29 -0500

    complete MVP for bold and italics

--- application.js
+++ application.js
@@ -13,18 +13,55 @@ function MarkDownWidget(source_id, preview_id){
     var text_array = text.split("");
 
     //iterate through text and change * to <i> or </i>
-    var counter = 0;
+    var bold_counter = 0;
+    var bold_counter2 = 0;
+    var italic_counter = 0;
+    var italic_counter2 = 0;
     for(var i=0; i < text_array.length; i++){
       var current_char = text_array[i];
 
-      if(current_char === "*" && counter === 0){
+      if(current_char === "*" && text_array[i + 1] === "*" && bold_counter === 0){
+        text_array[i] = "<str";
+        text_array[i+1] = "ong>";
+        bold_counter = 1;
+      }
+      else if(current_char === "*" && text_array[i + 1] === "*" && bold_counter === 1){
+        text_array[i] = "</str";
+        text_array[i+1] = "ong>";
+        bold_counter = 0;
+      }
+
+      else if(current_char === "_" && text_array[i + 1] === "_" && bold_counter2 === 0){
+        text_array[i] = "<str";
+        text_array[i+1] = "ong>";
+        bold_counter2 = 1;
+      }
+      else if(current_char === "_" && text_array[i + 1] === "_" && bold_counter2 === 1){
+        text_array[i] = "</str";
+        text_array[i+1] = "ong>";
+        bold_counter2 = 0;
+      }
+
+      else if(current_char === "*" && text_array[i + 1] != "*" && italic_counter === 0){
         text_array[i] = "<i>";
-        counter = 1;
+        italic_counter = 1;
       }
-      else if(current_char === "*" && counter === 1){
+      else if(current_char === "*" && text_array[i + 1] != "*" && italic_counter === 1){
         text_array[i] = "</i>";
-        counter = 0;
+        italic_counter = 0;
       }
+
+      else if(current_char === "_" && text_array[i + 1] != "_" && italic_counter2 === 0){
+        text_array[i] = "<i>";
+        italic_counter2 = 1;
+      }
+      else if(current_char === "_" && text_array[i + 1] != "_" && italic_counter2 === 1){
+        text_array[i] = "</i>";
+        italic_counter2 = 0;
+      }
+
+
+
     }
 
     //convertback to string
