877ecb38f6749891f33234244946d2bdd805fdae
michaelramuta & Shawn Wilkinson <pair+michaelramuta.shawndav@devbootcamp.com>
2015-07-29 11:30:14 -0500

    complete challenge

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var MarkdownWidget = function(input,output){
+    input.on("keyup",function(event){
+    var inputText = $(this).val();
+
+    var nth = 1;
+    inputText = inputText.replace(/\*{2}/g, function() {
+      nth++;
+      return (nth % 2 == 0) ? "<strong>" : "</strong>";
+    });
+
+    var nth = 1;
+    inputText = inputText.replace(/\*{1}/g, function() {
+      nth++;
+      return (nth % 2 == 0) ? "<em>" : "</em>";
+    });
+
+    var nth = 1;
+    inputText = inputText.replace(/\_{1}/g, function() {
+      nth++;
+      return (nth % 2 == 0) ? "<em>" : "</em>";
+    });
+
+    output.html(inputText);
+    });
+  };
+
+  var input = $("#input");
+  var output = $("#output");
+  MarkdownWidget(input,output);
+
 });
